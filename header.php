568729e3ab1d8cac9e9eea9c95ccbbfb65ecbb73
Ivan <iml_mendez@hotmail.es>
2021-07-04 20:46:32 +0200

    Change ID 23: added favicon

--- header.php
+++ header.php
@@ -4,33 +4,31 @@
  *
  */
 
-// ?> <DOCTYPE html>
-<html <?php language_attributes(); ?> >
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
 <head>
-<!--
     <meta charset="<?php bloginfo( 'charset' ); ?>">
-    <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1, maximum-scale=1" />
-    <link rel="manifest" href="<?php echo get_template_directory_uri() ?>/manifesto.json">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="manifest" href="<?php echo get_template_directory_uri()  . "/favicon"; ?>/manifesto.json">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link rel="profile" href="http://gmpg.org/xfn/11">
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-    <link rel="apple-touch-icon" sizes="57x57" href="<?php echo site_url() ?>/apple-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="<?php echo site_url() ?>/apple-icon-60x60.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="<?php echo site_url() ?>/apple-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="<?php echo site_url() ?>/apple-icon-76x76.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="<?php echo site_url() ?>/apple-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="<?php echo site_url() ?>/apple-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="<?php echo site_url() ?>/apple-icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="<?php echo site_url() ?>/apple-icon-152x152.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo site_url() ?>/apple-icon-180x180.png">
-    <link rel="icon" type="image/png" sizes="192x192"  href="<?php echo site_url() ?>/android-icon-192x192.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo site_url() ?>/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="96x96" href="<?php echo site_url() ?>/favicon-96x96.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo site_url() ?>/favicon-16x16.png">
+    <link rel="apple-touch-icon" sizes="57x57" href="<?php echo get_template_directory_uri()  . "/favicon";  ?>/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="<?php echo get_template_directory_uri()  . "/favicon";  ?>/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_template_directory_uri()  . "/favicon";  ?>/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_template_directory_uri()  . "/favicon" ?>/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri()  . "/favicon" ?>/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="<?php echo get_template_directory_uri()  . "/favicon"?>/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="<?php echo  get_template_directory_uri()  . "/favicon"?>/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="<?php echo  get_template_directory_uri()  . "/favicon" ?>/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo  get_template_directory_uri()  . "/favicon" ?>/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="<?php echo  get_template_directory_uri()  . "/favicon" ?>/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo  get_template_directory_uri()  . "/favicon" ?>/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="<?php echo  get_template_directory_uri()  . "/favicon" ?>/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo  get_template_directory_uri()  . "/favicon" ?>/favicon-16x16.png">
     <meta name="msapplication-TileColor" content="#ffffff">
-    <meta name="msapplication-TileImage" content="<?php echo site_url() ?>/ms-icon-144x144.png">
+    <meta name="msapplication-TileImage" content="<?php echo  get_template_directory_uri()  . "/favicon" ?>/ms-icon-144x144.png">
     <meta name="theme-color" content="#ffffff">
-    -->
 <?php wp_head(); ?>
 </head>
 
@@ -80,6 +78,6 @@
             </div>
         </div>
     <!-- </div> -->
-    </header><!-- #masthead
+    </header>
     <div id="content" class="site-content <?php echo $padding_site; ?>">
         
